8905a6ef8176a8f0d78d525d6a88aa16e3139bbc
jdollar <joel.dollarhide@passportinc.com>
2016-11-14 23:43:29 -0500

    Issues in current version of react. Snapshot testing for components are on hold. Creating a placeholder test file with information

---
+++ __tests__/components/HomeTest.js
@@ -0,0 +1,18 @@
+'use strict'
+
+import React from 'react'
+import Home from './../../src/js/components/Home'
+import renderer from 'react-test-renderer'
+
+/**
+ * Component Testing on hold for react 15.4.
+ * See https://github.com/facebook/react/issues/7386
+ **/
+//
+// test('Ensure home renders everything', () => {
+//   const homeComponent = renderer.create(
+//     <Home />
+//   )
+//   let tree = component.toJSON()
+//   expect(tree).toMatchSnapshot()
+// })
